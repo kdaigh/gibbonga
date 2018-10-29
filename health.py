8bdd7d7367af7d7237c03ab340641e50946c3f9f
Kristi Daigh <kdaigh20@gmail.com>
2018-10-29 11:31:36 -0500

    health.py: Updates to comments

--- health.py
+++ health.py
@@ -1,5 +1,5 @@
-## @file shot.py
-#  Source file for shot object
+## @file health.py
+#  Source file for health object
 #
 #  Project: Gallaga Clone
 #  Author: Py Five
@@ -9,13 +9,13 @@ import pygame
 from actor import Actor
 from player import Player
 
-## @class Shot
-#  @brief Implements Actor base class as Shot object
+## @class Health
+#  @brief Implements Actor base class as Health object
 class Health(Actor):
 
     ## Constructor
-    #  @param image, surface object with Shot image
-    #  @param player, Player object that fired the shot
+    #  @param image, surface object with Health image
+    #  @param player, Player object that will be linked with health
     def __init__(self, image, player):
         Actor.__init__(self, image)
         self.rect.centerx = player.rect.centerx
