f579c6839b7814628d59aff371d96a0cee9e2398
cmeyer <artgirlie26@gmail.com>
2018-11-16 09:29:19 -0600

    -1

--- game.py
+++ game.py
@@ -215,7 +215,7 @@ class Game:
             if not int(random.random() * const.ENEMY_SHOT_ODDS):
                 self.enemy_shot_count += 1
                 if (self.enemy_shot_count < const.MAX_ENEMY_SHOT):
-                    enemy_shots.append(Enemy_shot(enemy_shot_img, enemies[int(random.random() * len(enemies))]))
+                    enemy_shots.append(Enemy_shot(enemy_shot_img, enemies[int(random.random() * (len(enemies)-1))]))
             #i = i + 1
 
             for y in enemy_shots:
