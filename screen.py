c54c049d015951253d1505d0c6280d66c0152c09
Kristi Daigh <kdaigh20@gmail.com>
2018-11-18 14:12:20 -0600

    screen.py: Created basic screen class template.

---
+++ screen.py
@@ -0,0 +1,16 @@
+## @file screen.py
+#  Source file for screen base class
+#
+#  Project: Gallaga Clone
+#  Author: Py Five
+#  Created: 11/18/19
+
+# TBD: Import statements
+
+## @class Screen
+#  @brief Defines Screen base class
+class Screen():
+
+    ## TBD: Constructor
+    def __init__(self):
+        pass
