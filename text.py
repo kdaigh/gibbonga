4b77e2b2314d62012226b4d2f57663212051cbe2
Kristi Daigh <kdaigh20@gmail.com>
2018-11-19 12:39:15 -0600

    Updates to comments

--- text.py
+++ text.py
@@ -14,10 +14,10 @@ from actor import Actor
 class Text(Actor):
 
     ## Constructor
-    #  @param: text, text to be printed on screen
-    #  @param: color, color of text
-    #  @param: location, coordinates for text
-    #  @param: action, function to execute on click [OPTIONAL]
+    #  @param text, text to be printed on screen
+    #  @param color, color of text
+    #  @param location, coordinates for text
+    #  @param action, function to execute on click [OPTIONAL]
     def __init__(self, text, color, location, action=lambda x: None):
         self.text = text
         self.color = color
@@ -29,13 +29,13 @@ class Text(Actor):
         self.rect = self.image.get_rect(center=self.location)
 
     ## Updates the text, image, and coordinates of the text object
-    #  @param: text, text to be printed on screen
+    #  @param text, text to be printed on screen
     def update_text(self, text):
         self.text = text
         self.image = self.font.render(self.text, True, self.color)
         self.rect = self.image.get_rect(center=self.location)
 
     ## Executes on click function [if provided]
-    #  @pre: a function has been provided
+    #  @pre A function has been provided
     def on_click(self):
         self.action()
