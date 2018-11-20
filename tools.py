60a74bbba7dc02a70c490a5be97211d53ea362bb
Kristi Daigh <kdaigh20@gmail.com>
2018-11-19 21:58:20 -0600

    tools.py: Updates to comments

--- tools.py
+++ tools.py
@@ -1,6 +1,14 @@
+## @file tools.py
+#  Tools file containing game utility functions
+#
+#  Project: Gallaga Clone
+#  Author: Py Five
+#  Created: 11/19/19
+
 import os.path
 import pygame
 import constants as const
+
 
 ## Loads images for game into dictionary
 #  @pre File name has no periods in it
