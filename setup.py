f1b735d2ee3742883195574f5200fe92fd2e5f73
Kristi Daigh <kdaigh20@gmail.com>
2018-11-19 21:59:15 -0600

    setup.py: Updates to comments

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+## @file setup.py
+#  Setup file containing game asset dictionaries
+#
+#  Project: Gallaga Clone
+#  Author: Py Five
+#  Created: 11/19/19
+
 import os.path
 import pygame
 import tools
