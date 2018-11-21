f33323ca2e3a1fac7a96278547a232430622e145
Kristi Daigh <kdaigh20@gmail.com>
2018-11-21 12:19:24 -0600

    Removed cached files

---
+++ data/constants.py
@@ -0,0 +1,39 @@
+## @file constants.py
+#  File containing game constants
+#
+#  Project: Galaga Clone
+#  Author: Py Five
+#  Created: 10/18/18
+
+from pygame.locals import *
+
+
+# Frames Per Second
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Game Menu
+TEXT_FONT = "assets/fonts/arcadeclassic.ttf"
+TEXT_SIZE = 25
+
+# Window Size
+#SCREENRECT = Rect(0, 0, 640, 480)
+SCREENRECT = Rect(0, 0, 600, 650)
+
+# Player Speed
+PLAYER_SPEED = 12
+
+# Enemy
+ENEMY_SPEED = 4
+ENEMY_ODDS = 48 #was 24
+ENEMY_SIZE = (26, 26)
+MAX_ENEMY_SHOT = 150
+ENEMY_SHOT_ODDS = 50
+MAX_ENEMIES = 300
+
+# Shots
+MAX_SHOTS = 5
+SHOT_SPEED = 10
---
+++ data/setup.py
@@ -0,0 +1,16 @@
+## @file setup.py
+#  Setup file containing game asset dictionaries
+#
+#  Project: Galaga Clone
+#  Author: Py Five
+#  Created: 11/19/18
+
+import pygame, os.path
+from . import tools, constants
+
+
+pygame.init()
+screen = pygame.display.set_mode(constants.SCREENRECT.size, 0)
+
+IMAGES = tools.load_all_images(os.path.join("assets", "images"))
+SOUNDS = tools.load_all_sounds(os.path.join("assets", "audios"))
